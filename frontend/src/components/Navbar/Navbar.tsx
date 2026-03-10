c486c6299f1e965aa07e7ac534f932dde67487bc
Justin-K-Ellis <jlke45@gmail.com>
2026-03-10 18:04:17 +0900

    feat: create navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,18 +1,48 @@
-import Button from "../Button/Button";
+import { Link } from "react-router";
+import type { LinkInterface } from "../../types/interfaces";
 
 export default function Navbar() {
+  const links: LinkInterface[] = [
+    {
+      text: "Home",
+      url: "/",
+    },
+    {
+      text: "About",
+      url: "/about",
+    },
+    {
+      text: "Blog",
+      url: "/blog",
+    },
+  ];
+
   return (
-    <nav>
-      <button className="btn bnt-primary">Primary</button>
-      {/* <Button variant="primary" type="button">
-        Primary
-      </Button>
-      <Button variant="secondary" type="button">
-        Secondary
-      </Button>
-      <Button variant="accent" type="button">
-        Accent
-      </Button> */}
-    </nav>
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Justin Klitgaard</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <button className="btn">
+              <Link to={"/contact"}>Contact</Link>
+            </button>
+          </li>
+          <li className="mt-1 mx-1 md:mx-2">
+            <details>
+              <summary>More</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                {links.map((link) => (
+                  <li>
+                    <Link to={link.url}>{link.text}</Link>
+                  </li>
+                ))}
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
