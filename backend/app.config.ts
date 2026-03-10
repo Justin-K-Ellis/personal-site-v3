e8759fd9fe4dbc6913ebcade68f7a9577c595a16
Justin-K-Ellis <jlke45@gmail.com>
2026-03-10 18:06:13 +0900

    refactor: parseInt on port just once

--- backend/app.config.ts
+++ backend/app.config.ts
@@ -1,14 +1,13 @@
-import "dotenv/config";
-
 function getPort(): number {
   let PORT = process.env.PORT;
   if (!PORT) {
     throw new Error("Port not found.");
   }
-  if (isNaN(parseInt(PORT))) {
+  const port = parseInt(PORT);
+  if (isNaN(port)) {
     throw new Error("Port misconfigured (must be numeric).");
   }
-  return parseInt(PORT);
+  return port;
 }
 
 export { getPort };
