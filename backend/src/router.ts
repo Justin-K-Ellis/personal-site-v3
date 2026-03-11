6341e220ff493b7be793d96ecf217432e799c681
Justin-K-Ellis <jlke45@gmail.com>
2026-03-11 20:57:55 +0900

    feat: use factory pattern for controller

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
-
-import testController from "./controllers/test.controller.js";
+import makeTestController from "./controllers/test.controller.js";
+import { TestService } from "./services/test.service.js";
 
 const router = new Hono();
+const testController = makeTestController(new TestService());
 
 router.route("/test", testController);
 
