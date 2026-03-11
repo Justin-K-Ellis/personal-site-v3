621131605a4b49ba291a5524f37853a68f1d846a
Justin-K-Ellis <jlke45@gmail.com>
2026-03-11 21:00:52 +0900

    feat: add Hero section

---
+++ frontend/src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+
+export default function Hero() {
+  return (
+    <div className="hero bg-base-200 h-1/2 p-6">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Justin Klitgaard</h1>
+          <p className="py-6">
+            Welcome to my personal site. I'm a fullstack software developer
+            living and working in Tokyo. Check out my projects, tech stack, and
+            contact info below. You can learn a little more about me in the
+            About Me page.
+          </p>
+          <button className="btn btn-primary">
+            <Link to={"/contact"}>Contact Me</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
