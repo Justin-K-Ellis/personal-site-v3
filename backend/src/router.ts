495f450e6daea91baf51c12d88b070f156f30495
Justin-K-Ellis <jlke45@gmail.com>
2026-03-23 22:34:22 +0900

    fix: delete test route
    This controller and service are preserved in the test-routes branch.

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,10 +1,5 @@
 import { Hono } from "hono";
-import makeTestController from "./controllers/test.controller.js";
-import { TestService } from "./services/test.service.js";
 
 const router = new Hono();
-const testController = makeTestController(new TestService());
-
-router.route("/test", testController);
 
 export default router;
