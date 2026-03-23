2bc61dc6adef053f725ea73cda4a0acb88a1b6e4
Justin-K-Ellis <jlke45@gmail.com>
2026-03-23 23:11:53 +0900

    feat: add content for contact page

--- frontend/src/views/Contact.tsx
+++ frontend/src/views/Contact.tsx
@@ -1,3 +1,25 @@
+import Title from "../components/Title/Title";
+import contactData from "../assets/contactData";
+import ContactCard from "../components/ContactCard/ContactCard";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  const linkedin = contactData[0];
+  const github = contactData[1];
+
+  return (
+    <section
+      id="contact"
+      className="mt-4 md:mt-8 flex flex-col items-center gap-2"
+    >
+      <Title text="Contact" />
+      <p>You can get in touch with me here:</p>
+      <article className="flex justify-center mt-4 md:mt-8">
+        <ContactCard contact={github} />
+
+        <div className="divider divider-horizontal" />
+
+        <ContactCard contact={linkedin} />
+      </article>
+    </section>
+  );
 }
