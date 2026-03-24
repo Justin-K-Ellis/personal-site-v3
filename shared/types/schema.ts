bb84057df7653d305c71242e11f470081f3a290d
Justin-K-Ellis <jlke45@gmail.com>
2026-03-24 23:00:01 +0900

    add comment on example code

--- shared/types/schema.ts
+++ shared/types/schema.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 
+// == Example Code ==
 // Schema
 export const CatSchema = z.object({
   id: z.number(),
   name: z.string(),
 });
-
 export const NewCatSchema = CatSchema.omit({ id: true }).required();
-
 // Types
 export type Cat = z.infer<typeof CatSchema>;
 export type NewCat = z.infer<typeof NewCatSchema>;
