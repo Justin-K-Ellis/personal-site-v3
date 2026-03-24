d629ef56be95fb8d223bd1a87f138a1b14f34867
Justin-K-Ellis <jlke45@gmail.com>
2026-03-24 22:51:16 +0900

    feat: test resp body in healthCheck test

--- backend/src/tests/healthCheck.spec.ts
+++ backend/src/tests/healthCheck.spec.ts
@@ -2,9 +2,15 @@ import { describe, it, expect } from "vitest";
 import app from "../app.js";
 
 describe("the health check endpoint", () => {
+  const URL = "/health";
   it("should return status 200 from GET request", async () => {
-    const url = "/health";
-    const response = await app.request(url);
+    const response = await app.request(URL);
     expect(response.status).toBe(200);
+  });
+
+  it("should return the correct response body", async () => {
+    const response = await app.request(URL);
+    const body = await response.json();
+    expect(body).toEqual({ ok: true });
   });
 });
