6e8b78585f140e024c55a28a2dc782bd43077ae5
Justin-K-Ellis <jlke45@gmail.com>
2026-03-24 22:49:08 +0900

    feat: update gitignore

---
+++ frontend/src/assets/navLinks.ts
@@ -0,0 +1,18 @@
+import type { NavBarLink } from "../types/interfaces";
+
+const links: NavBarLink[] = [
+  {
+    text: "Home",
+    url: "/",
+  },
+  {
+    text: "About",
+    url: "/about",
+  },
+  {
+    text: "Blog",
+    url: "/blog",
+  },
+];
+
+export default links;
